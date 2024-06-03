70a993277e08b7f692ff646ed722587ebb28f7e8
lpanigrahi <89903479+lpanigrahi@users.noreply.github.com>
2024-06-03 22:26:33 +0530

    Update app_new_v2.py

--- app_new_v2.py
+++ app_new_v2.py
@@ -13,8 +13,8 @@ from langchain.output_parsers import StructuredOutputParser
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 import re
-from keywords import keywords
-nlp = spacy.load('en_core_web_lg')
+# from keywords import keywords
+# nlp = spacy.load('en_core_web_lg')
 
 import matplotlib.pyplot as plt
 from langchain.agents.agent_types import AgentType
