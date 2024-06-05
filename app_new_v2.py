5d1c8f0b6b3723797fbb7b96e8bc8c8aa74e3e0e
lpanigrahi <89903479+lpanigrahi@users.noreply.github.com>
2024-06-05 11:50:10 +0530

    Update app_new_v2.py

--- app_new_v2.py
+++ app_new_v2.py
@@ -29,7 +29,7 @@ def get_spreadsheet_summary(df, query):
     df,
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,
-    max_execution_time=200,
+    max_execution_time=2000,
     handle_parsing_errors= True
     )
     return agent.run(query)
